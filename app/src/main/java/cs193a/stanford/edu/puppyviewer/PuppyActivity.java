dba9ca6c33048b686088188080fa5b24b4c0808a
Michael Pauly <micscopau@gmail.com>
2017-10-30 15:17:16 -0600

    ButterKnife Library
    Attempted to implement ButterKnife Features:
    widget variable binding //my version caused app to crash with "global" declaration of img... not sure why
    spinner list on click listener method (to avoid android adapter) //lecture code and my version didnt work, prof not sure why.
    
    Next:
    Ion library - simplifies downloading files from web (beyond images that picasso provides)
    
    Follinwg lecture 9: on http://web.stanford.edu/class/cs193a/videos.shtml

--- app/src/main/java/cs193a/stanford/edu/puppyviewer/PuppyActivity.java
+++ app/src/main/java/cs193a/stanford/edu/puppyviewer/PuppyActivity.java
@@ -15,19 +15,55 @@ import com.nineoldandroids.animation.Animator;
 import com.nineoldandroids.animation.AnimatorListenerAdapter;
 import com.squareup.picasso.Picasso;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnItemSelected;
 import stanford.androidlib.*;
 
 public class PuppyActivity extends SimpleActivity {
     // constant for base web URL where image files are found
     private static final String WEBSITE_DIRECTORY = "http://www.martystepp.com/dogs/";
 
+    private static final String[] ALL_IMAGES = {
+            "barney-and-clyde-12.jpg",
+            "daisy-14.jpg",
+            "barney-and-clyde-02-large.jpg"
+    };
+
+    //@BindView(R.id.puppyphoto) ImageView img; //usefull if using a lot of findViewById
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puppy);
-        ImageView img = $IV(R.id.puppyphoto);
+        ButterKnife.bind(this);
+
+        SimpleList.with(this).setItems(R.id.puppy_spinner, ALL_IMAGES); //instead of using adaptor
+
+        //* Code being replaced by ButterKnife Library (see annotation onItemSelected :
+//        Spinner spin = (Spinner) findViewById(R.id.puppy_spinner);
+//        spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+//
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> adapterView) {
+//
+//            }
+//        });
+
     }
 
+    @OnItemSelected(R.id.puppy_spinner)
+    public void itemGotSelected(AdapterView<?> adapterView, View view, int i, long l){
+        String filename = ALL_IMAGES[i];
+        log("You clicked: " + filename);
+    }
+
+    @OnClick(R.id.clickme)
     public void clickMeClick(View view) {
         //widget effects
         YoYo.with(Techniques.Wobble)
@@ -43,8 +79,8 @@ public class PuppyActivity extends SimpleActivity {
     }
 
     private void displayImage(){
+        ImageView img = $IV(R.id.puppyphoto);
         //download and display
-
         Picasso.with(this)
                 .load("http://www.martystepp.com/dogs/barney-and-clyde-12.jpg")
                 .placeholder(R.drawable.loading)
