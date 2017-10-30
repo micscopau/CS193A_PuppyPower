897452d14227a696b5178a704b2af75a4a9feb49
Michael Pauly <micscopau@gmail.com>
2017-10-30 14:41:30 -0600

    Picasso Library and Animations Library added
    Can now download and display images from URL using Picasso Library.
    Animation actionas available from daimajia libraries using YoYo.
    
    Next:
    Display Image after animation has completed
    
    Follinwg lecture 9: on http://web.stanford.edu/class/cs193a/videos.shtml

--- app/src/main/java/cs193a/stanford/edu/puppyviewer/PuppyActivity.java
+++ app/src/main/java/cs193a/stanford/edu/puppyviewer/PuppyActivity.java
@@ -8,6 +8,11 @@ package cs193a.stanford.edu.puppyviewer;
 import android.os.*;
 import android.view.*;
 import android.widget.*;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import com.squareup.picasso.Picasso;
+
 import stanford.androidlib.*;
 
 public class PuppyActivity extends SimpleActivity {
@@ -18,5 +23,22 @@ public class PuppyActivity extends SimpleActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puppy);
+    }
+
+    public void clickMeClick(View view) {
+        //widget effects
+        YoYo.with(Techniques.Wobble)
+                .duration(5000)
+                .playOn(view);
+
+        // to perform an action after effect is completed, need to implement .withListener of YoYo
+
+
+        //download and display
+        ImageView img = $IV(R.id.puppyphoto);
+        Picasso.with(this)
+                .load("http://www.martystepp.com/dogs/barney-and-clyde-12.jpg")
+                .placeholder(R.drawable.loading)
+                .into(img);
     }
 }
